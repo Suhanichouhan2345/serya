f05b78496200bc4ab4b14392bba3125281c5c970
Suhanichauhan2345 <Suhanichouhan967@gamil.com>
2026-05-01 04:43:53 +0530

    push

--- precatice.js/index.js
+++ precatice.js/index.js
@@ -10,11 +10,15 @@ textContent: "Suhani you can do it"*/
 //h1.innerText ="Don't give up hanuman ji h tare sath :)"
  h1.innerHTML ="<i>Don't give up hanuman ji h tare sath :)</i>"
 //h1.textContent ="Don't give up hanuman ji h tare sath :)"
-let a = document.querySelector("a");
+//let a = document.querySelector("a");
 //console.dir(a);
 //a.href = "https://github.com/nitishyadav9893?tab=repositories"
 
-a.setAttribute("href", "https://github.com/nitishyadav9893?tab=repositories");
+//a.setAttribute("href", "https://github.com/nitishyadav9893?tab=repositories");
 
 let img = document.querySelector("img");
 img.setAttribute("src" ,"https://tse1.mm.bing.net/th/id/OIP.j63x_u2GiwM5fpjA3isTAwHaD_?pid=Api&P=0&h=180");
+
+let a = document.querySelector("a");
+//console.log(a.getAttribute("href"));
+a.removeAttribute("href");
---
+++ precatice.js/pectice1.js
@@ -0,0 +1,13 @@
+
+let button = document.querySelector("button");
+button.innerHTML = "<h1>Hello 😄</h1>";
+
+
+/*
+if(button.innerText = "mood"){
+    button.innerText = " ";
+}
+
+*/
+
+
